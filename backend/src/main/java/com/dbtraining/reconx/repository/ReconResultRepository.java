b28f7862066b4cd5c899d65840408b23af1addb5
pshavela <35176038+pshavela@users.noreply.github.com>
2026-07-27 20:45:15 +0200

    Fixed TICKET-ADV044 error causing spring-boot:run to fail, TradeService is only used for mock testing and should not be annotated with @Service

--- backend/src/main/java/com/dbtraining/reconx/repository/ReconResultRepository.java
+++ backend/src/main/java/com/dbtraining/reconx/repository/ReconResultRepository.java
@@ -3,6 +3,7 @@ package com.dbtraining.reconx.repository;
 
 import com.dbtraining.reconx.dto.ReconResult;
 
+// Only for Mock testing - TICKET-ADV044
 public interface ReconResultRepository {
     ReconResult save(ReconResult result);
 }
